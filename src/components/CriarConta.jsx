159f93f89f388006c35869897b2d0923f3c7baff
Maria Clara <clarachavesmc@gmail.com>
2025-03-27 00:02:14 -0300

    crianco sistema de autenticacao

--- src/components/CriarConta.jsx
+++ src/components/CriarConta.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import AuthContext from './AuthContext'// Certifique-se de que o caminho está correto
+import AuthContext from './AuthContext'
 import styles from "./CriarConta.module.css";
 
 function CriarConta() {
